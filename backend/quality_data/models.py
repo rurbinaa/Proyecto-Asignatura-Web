f48d85b6bdab67914febb87b030353b564164aa1
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-03-13 21:48:52 -0600

    feat(quality_data models): new attributee table_type using choices

--- backend/quality_data/models.py
+++ backend/quality_data/models.py
@@ -1,33 +1,18 @@
 from django.db import models
 
-# Create your models here.
 
-# Generic table for QC FA Plant and QC FA Customer
+class AmountDefects(models.Model):
 
-class QualityQcFa(models.Model):
-    # use proper Field instances with required args (max_length, etc.)
-    date_1 = models.CharField(max_length=50)
-    week = models.IntegerField()
-    customer = models.CharField(max_length=100)
-    team = models.IntegerField()
-    coord = models.CharField(max_length=50)
-    date_2 = models.CharField(max_length=50)
-    po = models.IntegerField()
-    style = models.CharField(max_length=100)
-    batch = models.IntegerField()
-    color = models.CharField(max_length=50)
-    qty = models.CharField(max_length=50)
-    seconds = models.IntegerField()
-    accepted = models.IntegerField()
-    rejected = models.IntegerField()
-    sample = models.IntegerField()
-    defeacts = models.IntegerField()
-    aql = models.CharField(max_length=50)
-    pass_or_fail = models.CharField(max_length=50)
-
-
-class amount_defects(models.Model):
+    table_type_choices ={
+        "QFA": "AC FA Plant",
+        "QFC": "QC FA Customer",
+        "S4": "SecondsA4",
+        "SG": "Seconds General",
+        "C": "Container"
+    }
     
+    table_type = models.CharField(max_length=1, choices=table_type_choices)
+
     uneven = models.IntegerField()
     broken_stitch = models.IntegerField()
     open_seam = models.IntegerField()
@@ -78,3 +63,28 @@ class amount_defects(models.Model):
     pocket_label = models.IntegerField()
     label_placement = models.IntegerField()
     missing_information_label = models.IntegerField()
+
+
+class QualityQcFa(models.Model):
+    date_1 = models.CharField(max_length=50)
+    week = models.IntegerField()
+    customer = models.CharField(max_length=100)
+    team = models.IntegerField()
+    coord = models.CharField(max_length=50)
+    date_2 = models.CharField(max_length=50)
+    po = models.IntegerField()
+    style = models.CharField(max_length=100)
+    batch = models.IntegerField()
+    color = models.CharField(max_length=50)
+    qty = models.CharField(max_length=50)
+    seconds = models.IntegerField()
+    accepted = models.IntegerField()
+    rejected = models.IntegerField()
+    sample = models.IntegerField()
+    defeacts = models.IntegerField()
+    aql = models.CharField(max_length=50)
+    pass_or_fail = models.CharField(max_length=50)
+
+    defeacts = models.ForeignKey(AmountDefects, on_delete=models.CASCADE)
+
+
