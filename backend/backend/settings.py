1297b138dca37029b909a6a284fc1e07eb626a2f
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-03-21 15:40:01 -0600

    test(quality_data): add tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,6 +87,14 @@ DATABASES = {
     }
 }
 
+# DB para test
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
