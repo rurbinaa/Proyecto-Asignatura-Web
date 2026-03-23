11e39c6411af4deec7ecaa9f9767bf8412501592
rurbinaa <ricardour2014@gmail.com>
2026-03-23 08:28:17 -0600

    feat(App): add state management for current defects and integrate onMarkersChange callback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import urlShirt from './assets/shirt.svg';
 import MockupContainer from './Components/mockupContainer.jsx';
 
 function App() {
+  
+  const [currentDefects, setCurrentDefects] = useState([]);
+  const handleDefectsUpdate = (updateMarkes) => {
+    setCurrentDefects(updateMarkes);
+
+    console.log('Updated defects:', updateMarkes);
+  };
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width:'100vw', minHeight:'60vh', padding:'20px' }}>
       
-      <MockupContainer garmentUrl={urlShirt} />
+      <MockupContainer 
+        garmentUrl={urlShirt} 
+        onMarkersChange={handleDefectsUpdate} 
+      />
       
     </div>
   );
